c2bbec185e821a3eab7e0d47b7456cc532dffd8b
clement-jny <clementjaunay@free.fr>
2025-02-11 22:48:52 +0100

    refactor: add message on login / register schema

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -1,17 +1,25 @@
 import { z } from 'zod';
 
-// TODO: message personnalisé
 export const registerSchema = z.object({
-  username: z.string().min(2).max(50),
-  firstname: z.string().min(2).max(50),
+  username: z
+    .string()
+    .min(2, { message: 'Your username must be 2 characters' })
+    .max(50),
+  firstname: z
+    .string()
+    .min(2, { message: 'Your firstname must be 2 characters' })
+    .max(50),
   pin: z.string().min(6, {
-    message: 'Your one-time password must be 6 characters.',
+    message: 'Your pin password must be 6 characters.',
   }),
 });
 
 export const loginSchema = z.object({
-  username: z.string().min(2).max(50),
+  username: z
+    .string()
+    .min(2, { message: 'Your username must be 2 characters' })
+    .max(50),
   pin: z.string().min(6, {
-    message: 'Your one-time password must be 6 characters.',
+    message: 'Your pin password must be 6 characters.',
   }),
 });
