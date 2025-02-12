a6c5a83211ceedbdf4bafe37526c19f13815d1ca
clement-jny <clementjaunay@free.fr>
2025-02-12 03:42:06 +0100

    feat: create invite schema

--- src/lib/zod-schemas.ts
+++ src/lib/zod-schemas.ts
@@ -24,5 +24,14 @@ export const loginSchema = z.object({
   }),
 });
 
+export const inviteSchema = z.object({
+  name: z.string().min(1, { message: 'The name must be 1 characters' }).max(50),
+  message: z
+    .string()
+    .min(1, { message: 'The message must be 1 characters' })
+    .max(255),
+});
+
 export type TRegisterSchema = z.infer<typeof registerSchema>;
 export type TLoginSchema = z.infer<typeof loginSchema>;
+export type TInviteSchema = z.infer<typeof inviteSchema>;
