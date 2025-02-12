d8036c5859d9f20d8bd17fb1cb34f21fc7929f7b
clement-jny <clementjaunay@free.fr>
2025-02-12 20:38:48 +0100

    feat(api): implement new type and return structured data

--- src/app/api/invite/route.ts
+++ src/app/api/invite/route.ts
@@ -6,10 +6,13 @@ import {
   updateInvitation,
 } from '@/actions/invite';
 import jwt from 'jsonwebtoken';
+import { type TApiCall } from '@/types/api-call';
 
-const secret = process.env.JWT_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET;
 
-export const GET = async (request: NextRequest) => {
+export const GET = async (
+  request: NextRequest
+): Promise<NextResponse<TApiCall>> => {
   const authToken = request.nextUrl.searchParams.get('authToken');
   const ref = request.nextUrl.searchParams.get('ref');
 
@@ -17,27 +20,43 @@ export const GET = async (request: NextRequest) => {
     try {
       if (!authToken)
         return NextResponse.json(
-          { error: 'Missing authToken' },
+          { success: false, message: 'Missing authToken' },
           { status: 400 }
         );
 
-      const decoded = jwt.verify(authToken, secret);
+      // TODO: fix to build
+      const decoded = jwt.verify(authToken, JWT_SECRET);
 
       const data = await getInvitationsByUserId(Number(decoded.userId));
 
-      return NextResponse.json({ data }, { status: 200 });
+      return NextResponse.json(
+        {
+          success: true,
+          message: 'Invitations fetched successfully',
+          data: { invitations: data },
+        },
+        { status: 200 }
+      );
     } catch (error) {
       console.error('Error:', error);
       return NextResponse.json(
-        { error: 'Failed to fetch invitations' },
+        { success: false, message: 'Failed to fetch invitations' },
         { status: 500 }
       );
     }
   } else if (ref) {
     const data = await getInvitationByRef(ref);
 
-    return NextResponse.json({ data }, { status: 200 });
+    return NextResponse.json(
+      { success: true, message: '', data },
+      { status: 200 }
+    );
   }
+
+  return NextResponse.json(
+    { success: false, message: 'Missing authToken or ref' },
+    { status: 400 }
+  );
 };
 
 export const POST = async (request: NextRequest) => {
@@ -47,7 +66,8 @@ export const POST = async (request: NextRequest) => {
     if (!name || !message)
       return NextResponse.json({ error: 'Missing fields' }, { status: 400 });
 
-    const decoded = jwt.verify(authToken, secret);
+    // TODO: fix to build
+    const decoded = jwt.verify(authToken, JWT_SECRET);
 
     await addInvitation(decoded.userId, name, message);
 
