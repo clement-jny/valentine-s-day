c83899ea1bfb9005264f07903d960ec0026ff654
clement-jny <clementjaunay@free.fr>
2025-02-12 05:21:34 +0100

    feat(api): update current route  & add new

--- src/app/api/invite/route.ts
+++ src/app/api/invite/route.ts
@@ -1,27 +1,42 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { getInvitationsByUserId, addInvitation } from '@/actions/invite';
+import {
+  getInvitationsByUserId,
+  addInvitation,
+  getInvitationByRef,
+  updateInvitation,
+} from '@/actions/invite';
 import jwt from 'jsonwebtoken';
 
 const secret = process.env.JWT_SECRET;
 
 export const GET = async (request: NextRequest) => {
-  try {
-    const authToken = request.nextUrl.searchParams.get('authToken');
+  const authToken = request.nextUrl.searchParams.get('authToken');
+  const ref = request.nextUrl.searchParams.get('ref');
 
-    if (!authToken)
-      return NextResponse.json({ error: 'Missing authToken' }, { status: 400 });
+  if (authToken) {
+    try {
+      if (!authToken)
+        return NextResponse.json(
+          { error: 'Missing authToken' },
+          { status: 400 }
+        );
 
-    const decoded = jwt.verify(authToken, secret);
+      const decoded = jwt.verify(authToken, secret);
 
-    const data = await getInvitationsByUserId(Number(decoded.userId));
+      const data = await getInvitationsByUserId(Number(decoded.userId));
+
+      return NextResponse.json({ data }, { status: 200 });
+    } catch (error) {
+      console.error('Error:', error);
+      return NextResponse.json(
+        { error: 'Failed to fetch invitations' },
+        { status: 500 }
+      );
+    }
+  } else if (ref) {
+    const data = await getInvitationByRef(ref);
 
     return NextResponse.json({ data }, { status: 200 });
-  } catch (error) {
-    console.error('Error:', error);
-    return NextResponse.json(
-      { error: 'Failed to fetch invitations' },
-      { status: 500 }
-    );
   }
 };
 
@@ -41,6 +56,25 @@ export const POST = async (request: NextRequest) => {
     console.error('Error:', error);
     return NextResponse.json(
       { error: 'Failed to create invitation' },
+      { status: 500 }
+    );
+  }
+};
+
+export const PATCH = async (request: NextRequest) => {
+  try {
+    const { ref, response } = (await request.json()) as {
+      ref: string;
+      response: 'yes' | 'no';
+    };
+
+    await updateInvitation(ref, response);
+
+    return NextResponse.json({ success: true }, { status: 200 });
+  } catch (error) {
+    console.error('Error:', error);
+    return NextResponse.json(
+      { error: 'Failed to update invitation' },
       { status: 500 }
     );
   }
